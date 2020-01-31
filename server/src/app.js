933a77ee53ae7933b305affe82bbc192f81db966
Besar Tasholli <bt43104@ubt-uni.net>
2020-01-31 16:52:09 +0100

    Vendosja e faqes se Regjistrimit

--- server/src/app.js
+++ server/src/app.js
@@ -1,17 +1,18 @@
 const express = require('express')
-const bodyParser = require('body-parser')
+const bodyParser = require ('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
 
 const app = express()
 app.use(morgan('combined'))
 app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
-
-app.get('/status', (req, res) => {
-   res.send({
-       message: 'Hello world!'
-   })
+app.post('/register', (req, res) => {
+res.send({
+    message:`hello ${req.body.email} Perdorusi juaj eshte regjistruar`
+    
+})
 })
 
 app.listen(process.env.PORT || 8081)
